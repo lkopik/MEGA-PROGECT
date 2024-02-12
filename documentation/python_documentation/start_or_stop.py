2e6a935ee196ebd5a87ba30eb1d0410232893a5c
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-13 00:24:11 +0500

    Create start_or_stop.py

---
+++ documentation/python_documentation/start_or_stop.py
@@ -0,0 +1,21 @@
+#версия 2.0 с выполнением bash скрипта. который вычитает значение из файла на сервере main.
+
+import os
+import RPi.GPIO as GPIO #Importe la bibliothèque pour contrôler les GPIOs
+
+os.system("bash /home/"NAME"/control_"NAME"/ssh_change_num.sh")
+
+GPIO.setmode(GPIO.BOARD) #Définit le mode de numérotation (Board)
+GPIO.setwarnings(False) #On désactive les messages d'alerte
+
+LED = 7 #Définit le numéro du port GPIO qui alimente la led
+
+GPIO.setup(LED, GPIO.OUT) #Active le contrôle du GPIO
+
+state = GPIO.input(LED) #Lit l'état actuel du GPIO, vrai si allumé, faux si éteint
+
+if state : #Si GPIO allumé
+    GPIO.output(LED, GPIO.LOW) #On l’éteint
+else : #Sinon
+    GPIO.output(LED, GPIO.HIGH) #On l'allume
+

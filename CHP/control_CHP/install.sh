40615e698ad98fd87789853de054ddcbac83ed57
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-13 13:09:22 +0500

    Update install.sh

--- CHP/control_CHP/install.sh
+++ CHP/control_CHP/install.sh
@@ -9,6 +9,20 @@ cd 8821cu-20210916
 sudo ./install-driver.sh
 
 
+#add a wifi repeter
+cat > /etc/systemd/system/apsys.service
+echo "[Unit]
+Description=apsys
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash -c "sleep 20 && create_ap --no-virt wlan1 wlan0 chp 12345678"
+
+[Install]
+WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/apsys.service
+systemctl deamon-reload
+systemctl enable apsys.service
+
 echo "[myshare]
 comment = Samba на Linux
 path = /home

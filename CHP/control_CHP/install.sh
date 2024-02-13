bf6d65ae4ed1f932f40ac57d3e456c8504459c41
lkopik <81908442+lkopik@users.noreply.github.com>
2024-02-13 12:57:38 +0500

    Update install.sh

--- CHP/control_CHP/install.sh
+++ CHP/control_CHP/install.sh
@@ -2,4 +2,10 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install samba samba-common-bin
 
-echo "" | sudo tee -a /etc/samba/smb.conf
+echo "[myshare]
+comment = Samba на Linux
+path = /home
+guest ok = yes
+read only = no
+browsable = yes
+writable = yes" | sudo tee -a /etc/samba/smb.conf
